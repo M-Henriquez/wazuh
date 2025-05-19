0bd1b1975ac4562cf77bbeba91c805d97022dfb1
M-Henriquez <mhenriquez@lbsteel.com>
2025-05-19 15:59:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ resource "azurerm_subnet" "subnet" {
     address_prefixes = ["10.0.1.0/24"]
 }
 
+// security group
 resource "azurerm_network_security_group" "nsg" {
     name = "wazuh-nsg"
     location = azurerm_resource_group.wazuh.location
